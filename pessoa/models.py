0c959c0f0ec9c1e95713543bd8d7a40a359824a3
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2022-02-05 10:55:17 -0300

    011

--- pessoa/models.py
+++ pessoa/models.py
@@ -6,7 +6,6 @@ class Pessoa(models.Model):
     nome_completo = models.CharField(max_length=256)
     data_nascimento = models.DateField(null=True)
     ativa = models.BooleanField(default=True)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     uf = models.CharField(max_length=2)
 
     def __str__(self):
